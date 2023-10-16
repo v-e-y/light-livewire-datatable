1fbd75fa57f2fe887b04ab1b1b06e36a7ef371b8
Edward V <v-e-y@outlook.com>
2023-10-16 21:20:01 +0300

    fix names, registr LW component

--- src/Providers/LightLivewireDataTableProvider.php
+++ src/Providers/LightLivewireDataTableProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace VEY\LightLivewireDataTable\Providers;
 
+use Livewire\Livewire;
 use Illuminate\Support\ServiceProvider;
 
 final class LightLivewireDataTableProvider extends ServiceProvider
@@ -11,5 +12,7 @@ final class LightLivewireDataTableProvider extends ServiceProvider
     public function boot(): void
     {
         $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'light-livewire-data-table');
+
+        Livewire::component('light-livewire-data-table', LightLivewireDataTable::class);
     }
 }
